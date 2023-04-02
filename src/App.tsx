ca8ed3eb357861199b33e449497052c16b7ac48a
Shin-sibainu <shincode0712@gmail.com>
2023-04-02 18:42:32 +0900

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 interface LoginForm {
   name: string;
@@ -8,15 +9,34 @@ interface LoginForm {
   password: string;
 }
 
+const validationSchema = z.object({
+  name: z
+    .string()
+    .nonempty("名前は必須です")
+    .min(4, "名前は4文字以上で入力してください"),
+  email: z
+    .string()
+    .nonempty("メールアドレスは必須です")
+    .email("正しいメールアドレスを入力してください"),
+  password: z
+    .string()
+    .nonempty("パスワードは必須です")
+    .min(6, "パスワードは6文字以上で入力してください"),
+});
+
 function App() {
   //フォーム状態とメソッドを取得
   const {
     register, //フォームから入力された値のstate管理、バリデーション処理が可能
     handleSubmit,
     formState: { errors },
-  } = useForm<LoginForm>({ mode: "onChange" });
+  } = useForm<LoginForm>({
+    mode: "onChange",
+    shouldUnregister: false,
+    resolver: zodResolver(validationSchema),
+  });
 
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: LoginForm) => {
     console.log("Submitted Data", data);
   };
 
@@ -28,10 +48,11 @@ function App() {
         <input
           type="text"
           id="name"
-          {...register("name", {
-            required: "名前は必須です",
-            minLength: { value: 4, message: "4文字以上で入力してください" },
-          })}
+          // {...register("name", {
+          //   required: "名前は必須です",
+          //   minLength: { value: 4, message: "4文字以上で入力してください" },
+          // })}
+          {...register("name")}
         />
         {errors.name && <p>{errors.name.message as React.ReactNode}</p>}
 
@@ -39,13 +60,14 @@ function App() {
         <input
           type="email"
           id="email"
-          {...register("email", {
-            required: "メールアドレスは必須です",
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-              message: "正しいメールアドレスを入力してください",
-            },
-          })}
+          // {...register("email", {
+          //   required: "メールアドレスは必須です",
+          //   pattern: {
+          //     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+          //     message: "正しいメールアドレスを入力してください",
+          //   },
+          // })}
+          {...register("email")}
         />
         {errors.email && <p>{errors.email.message as React.ReactNode}</p>}
 
@@ -53,10 +75,11 @@ function App() {
         <input
           id="password"
           type="password"
-          {...register("password", {
-            required: "パスワードは必須です",
-            minLength: { value: 8, message: "8文字以上で入力してください" },
-          })}
+          // {...register("password", {
+          //   required: "パスワードは必須です",
+          //   minLength: { value: 8, message: "8文字以上で入力してください" },
+          // })}
+          {...register("password")}
         />
         {errors.password && <p>{errors.password.message as React.ReactNode}</p>}
 
